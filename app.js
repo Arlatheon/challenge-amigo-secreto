f6c6ce51ecae70d2d408ad9190a00285820bb5de
Alexis Fernandez <arlatheontwich@gmail.com>
2025-07-29 18:17:08 -0300

    Agregando comentarios

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//!El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//?Creamos un array o lista llamado amigos.
 let amigos = []
+
+//?Creamos una funcion que al usuario darle click al boton de añadir y la caja este vacia, le salte una alerta de "Por favor, inserte un nombre".
 function agregarAmigo(){
+    //*Con document.querySelector llamamos a la id:"amigo" de html y le damos un valor, esto lo que hace es obtener el texto escrito por el usuario y almacenarlo en la variable "textoUsuario".
     let textoUsuario = document.querySelector("#amigo").value;
    
     if (textoUsuario == ""){
@@ -45,4 +50,8 @@ function sortearAmigo(){
     let result = document.querySelector("#resultado");
     result.innerHTML = `El Ganador del Sorteo es: ${amigoElegido}`
     limpiarCaja();
+
+    //Reiniciar el juego:
+    amigos = [];  // Vacia el array o lista.
+    listaAmigos(); // Actualiza la lista visible.
 }
